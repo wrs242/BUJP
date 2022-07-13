b4b330c79d17de5103bcc06f6de492c46cdd7f10
wrs242 <widekso242@gmail.com>
2022-07-13 11:32:00 +0700

    final anak magang

--- local_bujp.sql
+++ local_bujp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2022 at 05:10 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Generation Time: Jul 13, 2022 at 06:31 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -87,9 +87,9 @@ CREATE TABLE `bujp` (
   `KETERANGAN_SANKSI` char(100) DEFAULT NULL,
   `NAMA_DIREKTUR` char(100) DEFAULT NULL,
   `TELP_DIREKTUR` char(12) DEFAULT NULL,
-  `USER_CREATED` char(10) DEFAULT NULL,
+  `USER_CREATED` int(10) DEFAULT NULL,
   `DATE_CREATED` date DEFAULT NULL,
-  `USER_MODIFIED` char(10) DEFAULT NULL,
+  `USER_MODIFIED` int(10) DEFAULT NULL,
   `DATE_MODIFIED` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -99,12 +99,10 @@ CREATE TABLE `bujp` (
 
 INSERT INTO `bujp` (`ID_BUJP`, `ID_UNIT`, `NAMA_BUJP`, `TELP_KANTOR`, `TGL_TERIMA_COMPANY_PROFILE`, `ALAMAT`, `UNDANGAN_PRESENTASI`, `TGL_PRESENTASI`, `ALASAN_TIDAK_DIUNDANG`, `KEPEMILIKAN_KANTOR`, `PERIODE_SEWA`, `FOTO_KANTOR`, `AKTA_PENDIRIAN_PERUSAHAAN`, `TGL_PENDIRIAN`, `AKTA_PERUBAHAN`, `NO_NOTARIS`, `FOTO_AKTA`, `NO_SIO`, `MASA_BERLAKU_SIO`, `FOTO_SIO`, `STATUS_BPJS_KETENAGA_KERJAAN`, `NO_BPJS_KETENAGA_KERJAAN`, `FOTO_BPJS_KETENAGA_KERJAAN`, `STATUS_BPJS_KESEHATAN`, `NO_BPJS_KESEHATAN`, `FOTO_BPJS_KESEHATAN`, `FOTO_REKENING_KORAN`, `MOBIL_PATROLI`, `KEPEMILIKAN_MOBIL`, `MOTOR_PATROLI`, `KEPEMILIKAN_MOTOR`, `PORTO_MALL`, `PORTO_CLUSTER`, `PORTO_APARTEMEN`, `PORTO_PERKANTORAN`, `PORTO_KAWASAN`, `NAMA_PEJABAT_TNI`, `PANGKAT_PEJABAT_TNI`, `JABATAN_PEJABAT_TNI`, `RELASI_PEJABAT_TNI`, `FOTO_PEJABAT_TNI`, `NAMA_PEJABAT_POLRI`, `PANGKAT_PEJABAT_POLRI`, `JABATAN_PEJABAT_POLRI`, `RELASI_PEJABAT_POLRI`, `FOTO_PEJABAT_POLRI`, `SPT_PERUSAHAAN`, `HASIL_SELEKSI`, `STATUS`, `KETERANGAN_STATUS`, `OBJEK_PENGAMANAN`, `JUMLAH_PERSONEL`, `HARGA_PERORANG`, `TAKE_HOME_PAY_RUPIAH`, `TAKE_HOME_PAY_PERSEN`, `SANKSI`, `KETERANGAN_SANKSI`, `NAMA_DIREKTUR`, `TELP_DIREKTUR`, `USER_CREATED`, `DATE_CREATED`, `USER_MODIFIED`, `DATE_MODIFIED`) VALUES
 (1, NULL, 'PT. ABC', '0727845245', '2022-06-15', 'asdwd', 'Ya', NULL, NULL, NULL, NULL, '1655350106.png', 'Tidak', '2022-06-09', '2022-06-21', '10114410', '1655350106.png', '20144784', '2022-06-23', '1655350106.png', 'Terdaftar', 'awdwds', '1655350106.png', 'Terdaftar', '287050', '1655350106.png', '1655350106.png', 2, 'Milik Pribadi', 3, 'Milik Pribadi', 'ffdf', 'fffff', 'dddd', 'wqqqq', 'wwww', 'abc', 'qqq', 'www', 'eee', '1655350106.png', 'fdzwqw', 'qwqwqw', 'qwwqqw', 'sddsds', '1655350106.png', '1655350106.png', 'Lulus', 'Tidak Kerjasama', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(2, 1, 'PT. ABC', '121', '2022-06-15', 'asdwd', 'Ya', '2022-06-10', 'asdwds', NULL, '2022-06-15', '1655350281.png', 'Tidak', '2022-06-09', '2022-06-21', '10114410', '1655350281.png', '20144784', '2022-06-23', '1655350281.png', 'Terdaftar', 'awdwds', '1655350281.png', 'Terdaftar', '287050', '1655350281.png', '1655350281.png', 2, 'Milik Pribadi', 3, 'Milik Pribadi', 'ffdf', 'fffff', 'dddd', 'wqqqq', 'wwww', 'abc', 'qqq', 'www', 'eee', '1655350281.png', 'fdzwqw', 'qwqwqw', 'qwwqqw', 'sddsds', '1655350281.png', '1655350281.png', 'Lulus', 'Kerjasama', 'asdw', 'abx', '3', 2, 3, '4', 'SP 1', '4', '2', '3', NULL, NULL, NULL, NULL),
-(3, NULL, 'PT. ABC', '0727845245', '2022-06-15', 'asdwd', 'Ya', '2022-06-10', 'asdwds', NULL, '2022-06-15', '1655350486.png', 'Ya', '2022-06-09', '2022-06-21', '10114410', '1655350486.png', '20144784', '2022-06-23', '1655350486.png', 'Terdaftar', 'awdwds', '1655350486.png', 'Terdaftar', '287050', '1655350486.png', '1655350486.png', 2, 'Milik Pribadi', 3, 'Milik Pribadi', 'ffdf', 'fffff', 'dddd', 'wqqqq', 'wwww', 'abc', 'qqq', 'www', 'eee', '1655350486.png', 'fdzwqw', 'qwqwqw', 'qwwqqw', 'sddsds', '1655350486.png', '1655350486.png', 'Lulus', 'Kerjasama', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 1, 'PT. ABC', '2414', '2022-06-15', 'asdwd', 'Ya', '2022-06-10', 'asdwds', NULL, '2022-06-15', '1655350281.png', 'Tidak', '2022-06-09', '2022-06-21', '10114410', '1655350281.png', '20144784', '2022-06-23', '1655350281.png', 'Terdaftar', 'awdwds', '1655350281.png', 'Terdaftar', '287050', '1655350281.png', '1655350281.png', 2, 'Milik Pribadi', 3, 'Milik Pribadi', 'ffdf', 'fffff', 'dddd', 'wqqqq', 'wwww', 'abc', 'qqq', 'www', 'eee', '1655350281.png', 'fdzwqw', 'qwqwqw', 'qwwqqw', 'sddsds', '1655350281.png', '1655350281.png', 'Lulus', 'Kerjasama', 'wdasdw', 'sdawcc', '4', 3456235, 341412, '10', 'SP 1', '4', 'asfwf', '334234', NULL, NULL, NULL, NULL),
 (4, 3, 'PT. XYZ', '231231', '2022-06-22', 'Serpong Terrace Blok E.5/1', 'Ya', '2022-06-16', 'asdwds', 'Ya', '2022-06-22', '1655450177.png', 'Ya', '2022-06-23', '2022-06-14', '4174175', '1655450177.png', '20144784', '2022-06-14', '1655450177.png', 'Terdaftar', '052452', '1655450177.png', 'Tidak Terdaftar', '287050', '1655450177.png', '1655450177.png', 2, 'Milik Pribadi', 2, 'Milik Pribadi', 'aaaa', 'ssss', 'dddd', 'fff', 'ggg', 'azz', 'azaza', 'aaza', 'azaza', '1655450177.png', 'asdas', 'wdwdw', 'qwwqqw', 'sddsds', '1655450177.png', '1655450177.png', 'Lulus', 'Kerjasama', 'wdasdw', 'sada', '2', 1123123, 123, '10', 'SP 1', '231', 'asdawdw', '232323', NULL, NULL, NULL, NULL),
 (5, 1, 'PT. ABC', '121', '2022-06-15', 'asdwd', 'Ya', '2022-06-10', 'asdwds', NULL, '2022-06-15', '1655350281.png', 'Tidak', '2022-06-09', '2022-06-21', '10114410', '1655350281.png', '20144784', '2022-06-23', '1655350281.png', 'Terdaftar', 'awdwds', '1655350281.png', 'Terdaftar', '287050', '1655350281.png', '1655350281.png', 2, 'Milik Pribadi', 3, 'Milik Pribadi', 'ffdf', 'fffff', 'dddd', 'wqqqq', 'wwww', 'abc', 'qqq', 'www', 'eee', '1655350281.png', 'fdzwqw', 'qwqwqw', 'qwwqqw', 'sddsds', '1655350281.png', '1655350281.png', 'Lulus', 'Kerjasama', 'asdw', 'ABC', '3', 2, 3, '4', 'SP 1', '4', '2', '3', NULL, NULL, NULL, NULL),
-(6, 3, 'PT. abdefg', 'wdw', '2022-06-16', 'fwefasdw', 'Ya', '2022-06-16', 'awdsw', 'Ya', '2022-06-14', '1656061474.png', 'Ya', '2022-06-23', '2022-06-29', '10114410', '1656061474.png', '245285', '2022-06-23', '1656061474.png', 'Terdaftar', '5242\\', '1656061474.png', 'Terdaftar', '14152', '1656061474.png', '1656061474.png', 1, 'Milik Pribadi', 4, 'Milik Pribadi', '52', '4524', '52', '524', '5245', '525', '542', '42', '52', '1656061474.png', '5245', '525', '52452', '525', '1656061474.png', '1656061474.png', 'Lulus', 'Kerjasama', '313', 'gvgvg', '3', 200000, 238763423, '231', 'SP 1', '231', 'wdasd', 'awds', NULL, NULL, NULL, NULL),
-(7, NULL, 'Pradita University', '0727845245', '2022-06-21', 'Serpong Terrace Blok E.5/1', 'Ya', '2022-06-29', 'asdwds', 'Ya', NULL, '1656402078.png', 'Ya', '2022-06-29', '2022-06-30', '10114410', '1656402078.png', '20144784', '2022-06-29', '1656402078.png', 'Terdaftar', '5242\\', '1656402078.png', 'Terdaftar', '14152', '1656402078.png', '1656402078.png', 2, 'Milik Pribadi', 2, 'Sewa', 'ffdf', 'wdwdw', 'dddd', 'wqqqq', 'wwww', 'abc', 'qqq', 'www', 'eee', '1656402078.png', 'fdzwqw', 'qwqwqw', 'qwwqqw', 'sddsds', '1656402078.png', '1656402078.png', 'Lulus', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(7, NULL, 'Pradita University', '0727845245', '2022-06-21', 'Serpong Terrace Blok E.5/1', 'Ya', '2022-06-29', 'asdwds', 'Ya', NULL, '1656402078.png', 'Ya', '2022-06-29', '2022-06-30', '10114410', '1656402078.png', '20144784', '2022-06-29', '1656402078.png', 'Terdaftar', '5242\\', '1656402078.png', 'Terdaftar', '14152', '1656402078.png', '1656402078.png', 2, 'Milik Pribadi', 2, 'Sewa', 'ffdf', 'wdwdw', 'dddd', 'wqqqq', 'wwww', 'abc', 'abc', 'www', 'eee', '1656402078.png', 'fdzwqw', 'qwqwqw', 'qwwqqw', 'sddsds', '1656402078.png', '1656402078.png', 'Lulus', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 8, '2022-06-29');
 
 -- --------------------------------------------------------
 
@@ -149,7 +147,7 @@ INSERT INTO `kontrak` (`ID_BUJP`, `ID_KONTRAK`, `NO_SPK`, `UPLOAD_SPK`, `AWAL_KO
 (1, 1, '123123', NULL, '2022-06-23', '2022-07-01', '2', '2', 'Adendum', 'wdwa'),
 (1, 2, '2231', NULL, '2022-06-24', '2022-06-29', '2', '1', 'Adendum', 'we1e1'),
 (1, 3, '2231231', NULL, '2022-06-24', '2022-07-07', '3', '2', 'Adendum', '3232'),
-(6, 4, '2231231', NULL, '2022-06-22', '2022-06-30', '1', '1', 'Adendum', '3342');
+(2, 5, '25453', NULL, '2022-06-13', '2022-06-30', '1', '1', 'Adendum', 'dadwaw');
 
 -- --------------------------------------------------------
 
@@ -235,12 +233,18 @@ CREATE TABLE `password_resets` (
 --
 
 CREATE TABLE `pedoman_keamanan` (
-  `ID_PEDOMAN` char(10) NOT NULL,
-  `NAMA_PEDOMAN` char(50) DEFAULT NULL,
-  `FILE_PEDOMAN` longblob DEFAULT NULL,
-  `USER_MODIFIED` char(10) DEFAULT NULL,
+  `ID_PEDOMAN` int(10) NOT NULL,
+  `FILE_PEDOMAN` char(100) DEFAULT NULL,
+  `USER_MODIFIED` int(10) DEFAULT NULL,
   `DATE_MODIFIED` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pedoman_keamanan`
+--
+
+INSERT INTO `pedoman_keamanan` (`ID_PEDOMAN`, `FILE_PEDOMAN`, `USER_MODIFIED`, `DATE_MODIFIED`) VALUES
+(6, 'standarisasi_kompetensi.pdf', 8, '2022-06-30');
 
 -- --------------------------------------------------------
 
@@ -362,7 +366,8 @@ CREATE TABLE `stakeholder` (
 
 INSERT INTO `stakeholder` (`ID_STAKEHOLDER`, `KAWASAN`, `WILAYAH`, `INSTANSI`, `WILAYAH_HUKUM`, `ALAMAT_INSTANSI`, `NAMA_P_LAMA`, `PANGKAT_P_LAMA`, `MULAI_MENJABAT_P_LAMA`, `AKHIR_MENJABAT_P_LAMA`, `JUMLAH_PERSONEL_P_LAMA`, `LOKASI_TUGAS_BARU_P_LAMA`, `TAHUN_ANGKATAN_P_LAMA`, `TELP_P_LAMA`, `FOTO_P_LAMA`, `NAMA_P_BARU`, `PANGKAT_P_BARU`, `MULAI_MENJABAT_P_BARU`, `AKHIR_MENJABAT_P_BARU`, `JUMLAH_PERSONEL_P_BARU`, `LOKASI_TUGAS_LAMA_P_BARU`, `TAHUN_ANGKATAN_P_BARU`, `TELP_P_BARU`, `FOTO_P_BARU`, `USER_CREATED`, `DATE_CREATED`, `USER_UPDATED`, `DATE_UPDATED`) VALUES
 (5, 'Summarecon Kelapa Gading', 'AAAAAA', 'POLRI', 'asdwds', 'Serpong Terrace Blok E.5/1', 'dasdw', 'asddwa', '2022-06-28', '2022-06-29', '50', 'adw', 'asdw', 'sssss', '1656316297.jpg', 'asdw', 'wdwdwd', '2022-07-01', '2022-06-20', 'a', 'sdwdas', '2021', 'aaaaa', '1656316297.jpg', 8, '2022-06-27', 10, '2022-06-27'),
-(6, 'Summarecon Kelapa Gading', 'sdwds', 'POLRI', 'wdasdw', 'Serpong Terrace Blok E.5/1', 'wdwdw', 'asddwa', '2022-06-06', '2022-06-21', '20', 'dwda', NULL, NULL, '1656314569.jpg', 'wdwa', 'wdwdwd', '2022-06-27', '2022-06-06', 'dwd', 'awd', '2020', 'wdw', '1656314570.jpg', 10, '2022-06-27', NULL, NULL);
+(6, 'Summarecon Kelapa Gading', 'sdwds', 'POLRI', 'wdasdw', 'Serpong Terrace Blok E.5/1', 'wdwdw', 'asddwa', '2022-06-06', '2022-06-21', '20', 'dwda', NULL, NULL, '1656314569.jpg', 'wdwa', 'wdwdwd', '2022-06-27', '2022-06-06', 'dwd', 'awd', '2020', 'wdw', '1656314570.jpg', 10, '2022-06-27', NULL, NULL),
+(10, 'Summarecon Kelapa Gading', 'wdwdwd', 'POLRI', 'wwwwwwww', 'Serpong Terrace Blok E.5/1', 'wdasw', 'sdwd', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'wdwaswdwd', 'wdasw', '2022-07-19', '2022-06-27', '5', NULL, 'wdas', 'wdws', '1656948899.jpg', 8, '2022-07-04', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -385,30 +390,6 @@ INSERT INTO `unit` (`ID_UNIT`, `NAMA_UNIT`, `USER_CREATED`, `DATE_CREATED`) VALU
 (1, 'Summarecon Serpong', NULL, NULL),
 (2, 'Summarecon Bogor', NULL, NULL),
 (3, 'Summarecon Kelapa Gading', NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` char(10) NOT NULL,
-  `NAMA_USER` char(100) DEFAULT NULL,
-  `ROLE` char(10) DEFAULT NULL,
-  `username` char(100) DEFAULT NULL,
-  `password` char(100) NOT NULL,
-  `USER_CREATED` char(10) DEFAULT NULL,
-  `DATE_CREATED` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `NAMA_USER`, `ROLE`, `username`, `password`, `USER_CREATED`, `DATE_CREATED`) VALUES
-('1', 'admin', 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', '2022-06-03'),
-('2', '123', 'admin', '123', '123', '', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -533,12 +514,6 @@ ALTER TABLE `unit`
   ADD KEY `USER_CREATED` (`USER_CREATED`);
 
 --
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -553,7 +528,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bujp`
 --
 ALTER TABLE `bujp`
-  MODIFY `ID_BUJP` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID_BUJP` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -565,7 +540,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `kontrak`
 --
 ALTER TABLE `kontrak`
-  MODIFY `ID_KONTRAK` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID_KONTRAK` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `kontrol_bulanan`
@@ -578,6 +553,12 @@ ALTER TABLE `kontrol_bulanan`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pedoman_keamanan`
+--
+ALTER TABLE `pedoman_keamanan`
+  MODIFY `ID_PEDOMAN` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `penilaian_tahunan`
@@ -595,7 +576,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `stakeholder`
 --
 ALTER TABLE `stakeholder`
-  MODIFY `ID_STAKEHOLDER` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID_STAKEHOLDER` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `unit`
@@ -617,9 +598,9 @@ ALTER TABLE `users`
 -- Constraints for table `bujp`
 --
 ALTER TABLE `bujp`
-  ADD CONSTRAINT `bujp_ibfk_1` FOREIGN KEY (`USER_CREATED`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `bujp_ibfk_2` FOREIGN KEY (`USER_MODIFIED`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `bujp_ibfk_3` FOREIGN KEY (`ID_UNIT`) REFERENCES `unit` (`ID_UNIT`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `bujp_ibfk_3` FOREIGN KEY (`ID_UNIT`) REFERENCES `unit` (`ID_UNIT`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bujp_ibfk_4` FOREIGN KEY (`USER_CREATED`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `bujp_ibfk_5` FOREIGN KEY (`USER_MODIFIED`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `kontrak`
@@ -631,22 +612,18 @@ ALTER TABLE `kontrak`
 -- Constraints for table `kontrol_bulanan`
 --
 ALTER TABLE `kontrol_bulanan`
-  ADD CONSTRAINT `kontrol_bulanan_ibfk_1` FOREIGN KEY (`USER_CREATED`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `kontrol_bulanan_ibfk_2` FOREIGN KEY (`USER_MODIFIED`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `kontrol_bulanan_ibfk_3` FOREIGN KEY (`ID_BUJP`) REFERENCES `bujp` (`ID_BUJP`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `pedoman_keamanan`
 --
 ALTER TABLE `pedoman_keamanan`
-  ADD CONSTRAINT `pedoman_keamanan_ibfk_1` FOREIGN KEY (`USER_MODIFIED`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `pedoman_keamanan_ibfk_1` FOREIGN KEY (`USER_MODIFIED`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `penilaian_tahunan`
 --
 ALTER TABLE `penilaian_tahunan`
-  ADD CONSTRAINT `penilaian_tahunan_ibfk_1` FOREIGN KEY (`USER_CREATED`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `penilaian_tahunan_ibfk_2` FOREIGN KEY (`USER_MODIFIED`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `penilaian_tahunan_ibfk_3` FOREIGN KEY (`ID_BUJP`) REFERENCES `bujp` (`ID_BUJP`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
@@ -660,12 +637,6 @@ ALTER TABLE `perlengkapan_kontrol_bulanan`
 --
 ALTER TABLE `stakeholder`
   ADD CONSTRAINT `stakeholder_ibfk_1` FOREIGN KEY (`USER_CREATED`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `unit`
---
-ALTER TABLE `unit`
-  ADD CONSTRAINT `unit_ibfk_1` FOREIGN KEY (`USER_CREATED`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
